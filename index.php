04884d3994caa794cd512b6959e43c38cb030798
Geechl <actionher@gmail.com>
2019-02-04 08:11:19 +0200

    next

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>
+        web
+    </title>
+</head>
+<body>
+<form action="database.php" method="post">
+    <input type="text" name="add">
+    <input type="text" name="read">
+</form>
+</body>
+</html>

f6450ccc67da8184c7902cdda77dab57bc8bace2
Geechl <actionher@gmail.com>
2019-02-06 05:29:26 +0200

    INEET

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+ class data {
+     public function add (){
+
+     }
+ }
--- index.php
+++ index.php
@@ -4,11 +4,25 @@
     <title>
         web
     </title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudfla re.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+
 </head>
 <body>
+
 <form action="database.php" method="post">
-    <input type="text" name="add">
-    <input type="text" name="read">
+
+    <p>Name <input type="text" name="name"></p><br />
+    <p>Model <input type="text" name="model"></p><br />
+    <p>Country <input type="text" name="country"></p><br  />
+    <input type="submit" name="create" value="Create">
+    <input type="submit" name="read" value="Read">
+    <input type="submit" name="delete" value="Delete">
+    <input type="submit" name="update" value="Update">
 </form>
+
 </body>
 </html>
